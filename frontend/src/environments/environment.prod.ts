0cd4b385d051185d4868feaa59d2fabcf5e9e8c6
dglink25 <dglink25@gmail.com>
2026-09-15 17:04:39 +0100

    Mise à jours url backend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Remplacer par l'URL de l'API déployée (ex. Railway/Render)
-  apiUrl: 'https://api.fluxo.app/api',
+  apiUrl: 'https://fluxo-gmtz.onrender.com',
 };
